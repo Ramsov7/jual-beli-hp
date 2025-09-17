5c47156f1ac0b0e021c4c532ee046494a21a2ec2
Ramsov7 <ramsov7@gmail.com>
2025-09-18 03:27:36 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ const sections = document.querySelectorAll("main section");
 
 navButtons.forEach(btn => {
   btn.addEventListener("click", () => {
-    // Hapus aktif sebelumnya
+    // reset aktif
     navButtons.forEach(b => b.classList.remove("active"));
     sections.forEach(s => s.classList.remove("active"));
 
-    // Aktifkan yang dipilih
+    // aktifkan
     btn.classList.add("active");
     document.getElementById(btn.dataset.target).classList.add("active");
   });
